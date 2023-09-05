729388db8fd10271ef27047f7646478ac56fecdd
Aves <carlier.amandine98@gmail.com>
2023-09-05 12:30:12 +0200

    feat(grid): wip

--- lib/pages/Game.dart
+++ lib/pages/Game.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:compilation_et_dev_mobile/components/Grid.dart';
+import 'package:flutter/material.dart';
 
 class Game extends StatefulWidget {
   const Game({Key? key, required this.title}) : super(key: key);
@@ -12,7 +13,125 @@ class Game extends StatefulWidget {
 class _GameState extends State<Game> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.white,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                style: ButtonStyle(
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.black),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text(
+                  'Return',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Text(
+                widget.title,
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                width: 25,
+              ),
+            ],
+          )),
+      backgroundColor: const Color(0xFF03A9F4),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Align(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                height: 25,
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              SizedBox(
+                width: 150.0,
+                height: 75.0,
+                child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    // TODO
+                    Navigator.pushNamed(
+                      context,
+                      '/game',
+                    );
+                  },
+                  child: const Text(
+                    'New grid',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+              // TODO Grid
+              SizedBox(
+                  child: Grid()
+              ),
+              const SizedBox(height: 25),
+              SizedBox(
+                width: 150.0,
+                height: 75.0,
+                child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.black),
+                  ),
+                  onPressed: () {
+                    // TODO
+                    Navigator.pushNamed(
+                      context,
+                      '/game',
+                    );
+                  },
+                  child: const Text(
+                    'Check',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 25),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
